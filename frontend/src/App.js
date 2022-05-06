79d3d493c3f0f1261e269170dff0146020b0da42
GonSBayo <gsanchezbayo@gmail.com>
2022-05-06 19:41:08 +0200

    Encriptar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import TFGList from './TFGList';
 import TFGEdit from "./TFGEdit";
 import Login from './login';
+import Registro from './registro';
+
 
 class App extends Component {
   render() {
     return (
         <Router>
           <Switch>
-            <Route path='/' exact={true} component={Home}/>
+            <Route path='/' exact={true} component={Registro}/>
             <Route path='/tfgs' exact={true} component={TFGList}/>
             <Route path='/tfgs/:email' component={TFGEdit}/>
           </Switch>
---
+++ frontend/src/registro.js
@@ -0,0 +1,110 @@
+import React, { Component, useState, useEffect } from 'react';
+import './style.css';
+import { Link } from 'react-router-dom';
+import { Button, Container } from 'react-bootstrap';
+import {CryptoJS} from 'crypto-js';
+
+
+function Registro() {
+
+    const EMAIL = "";
+    const PASSWORD = "";
+    const NAME ="";
+
+    const [email, setEmail]= useState(EMAIL);
+    const [password, setPassword]= useState(PASSWORD);
+    const [name, setName]= useState(NAME);
+
+
+
+    async function register(){
+        var CryptoJS=require("crypto-js");
+        var result = CryptoJS.AES.encrypt(JSON.stringify(password), 'KeyISST').toString();
+        var log = {"name":name, "email": email, "pass": result, "pedidos": "", "compañias": "" }
+        await fetch('/ships', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(log)
+        });
+        // setPassword(result);
+        setEmail("");
+        setPassword("");
+        setName("");
+    }
+
+    function decrypt(){
+        var CryptoJS=require("crypto-js");
+        var bytes = CryptoJS.AES.decrypt(password, 'KeyISST');
+        var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+        setPassword(decryptedData);
+    }
+
+    return (
+
+        <div class="login">
+
+            <h2><img src={process.env.PUBLIC_URL + "/images/Logo.png"} ></img></h2>
+
+            <div class="container" id="container">
+
+                    <br></br>
+                
+
+                    <h2 align="center">Registrarse</h2>
+                                        
+                    <form>
+                        <label>
+                            <input type = "text" name ="name" placeholder='Nombre' value={name} onChange={e=>setName(e.target.value)}/>
+                        </label>
+                            
+
+                    </form>
+
+                    <form>
+                        <label>
+                            <input type = "text" name ="name" placeholder='Email' value={email} onChange={e=>setEmail(e.target.value)}/>
+                        </label>
+                            
+
+                    </form>
+
+ 
+
+
+                    <form>
+                        <label>
+                            <input type="text" name="name" placeholder='Contraseña' value={password} onChange={e=>setPassword(e.target.value)}/>
+                        </label>
+                    </form>
+
+                    <h2>
+                        <Button onClick={()=>register()} align="center"> Regístrate</Button>   
+                    </h2>
+
+                    <br></br>
+                    <br></br>
+                    <br></br>
+
+
+                    <h2>¿Ya estas registrado?</h2>
+                    <h5>Inicia sesión con tu cuenta</h5>
+                    <h2>
+                        <Button onClick={()=>decrypt()}  align="center">Iniciar sesión</Button>
+                    </h2>
+                    
+ 
+
+
+
+               
+  
+            </div>
+        </div>    
+
+    );
+}
+
+export default Registro;
